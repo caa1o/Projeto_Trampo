7965ff13ecd299004102b24f90bbd9235f1ef195
Marcoantnascimento <143298232+Marcoantnascimento@users.noreply.github.com>
2023-10-25 23:17:58 -0300

    Ajustes
    Páginas dos trabalhadores feitas, e responsividade.

---
+++ scripts/header.js
@@ -0,0 +1,3 @@
+let cabecalho = document.getElementsByClassName('cabecalho_dinamico')[0]
+
+cabecalho.innerHTML='<div><div class="logoHeader"><a href="index.html" draggable="false"><img class="logo" src="mainpage/img/logo.png" draggable="false"></a><h2 class="logoFrase">Onde o trabalhador encontra serviço</h2></div></div>'
