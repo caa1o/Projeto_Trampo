b0b157407629d48a8be55fb6d3cdf9d42c9fa849
Kleyton Holanda <69331002+Kehs4@users.noreply.github.com>
2023-11-14 20:31:25 -0300

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,3 +1,3 @@
 let cabecalho = document.getElementsByClassName('cabecalho_dinamico')[0]
 
-cabecalho.innerHTML='<div><div class="logoHeader"><a href="index.html" draggable="false"><img class="logo" src="mainpage/img/logo.png" draggable="false"></a><h2 class="logoFrase">Onde o trabalhador encontra serviço</h2></div></div>'
+cabecalho.innerHTML='<div><div class="logoHeader"><a href="index.html" draggable="false"><img class="logo" src="mainpage/img/logo.png" draggable="false"></a></div></div>'
